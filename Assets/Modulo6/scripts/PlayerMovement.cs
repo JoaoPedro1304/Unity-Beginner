fd83e8ec278f7783953cc8e53e28d4ae6a95e3f7
Joao Pedro da Costa Assis <joao.ca@pti.org.br>
2023-09-05 15:46:06 -0300

    Adicionado Assets Módulo 6

---
+++ Assets/Modulo6/scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int speed;
+    public Vector3 movePosition;
+    void Start()
+    {
+        speed = 5;
+    }
+
+
+    void Update()
+    {
+       movePosition = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
+
+        
+        transform.position += movePosition * speed * Time.deltaTime;
+        
+
+        
+    }
+    
+    
+}
