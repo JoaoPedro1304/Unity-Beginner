d41c4963bf4d346c53cab93343e156772e84c2c7
Joao Pedro da Costa Assis <joao.ca@pti.org.br>
2023-09-13 13:39:51 -0300

    Subistituido scripts com modificação  modulo 7

--- Assets/Modulo 7/scripts/EndGame.cs
+++ Assets/Modulo 7/scripts/EndGame.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndGame : MonoBehaviour
 {    
     
     void OnTriggerEnter(Collider collider){
 
-        if(collider.gameObject.tag =="CanCollide"){         
-            collider.gameObject.GetComponent<PlayerMove>().enabled = false;          
+        if(collider.gameObject.tag =="CanCollide"){
+
+            collider.gameObject.GetComponent<PlayerMove>().enabled = false;
+
             Time.timeScale = 0;
-        }        
+            
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+            Time.timeScale = 1;
+
+        }
 
     }
 }
